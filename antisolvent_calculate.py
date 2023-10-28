aad3c8a257fb6dd953b925eece697459cbb792c8
Hyuntae Park <proHyuntae@gmail.com>
2023-10-28 18:03:45 +0900

    Add create system and calc ternary data methods

--- antisolvent_calculate.py
+++ antisolvent_calculate.py
@@ -3,7 +3,6 @@ from numpy.typing import ArrayLike
 import pandas
 import matplotlib.pyplot as plt
 import ternary
-from typing import Type
 
 from cosmosac2 import COSMOMolecule, COSMOSAC
 from ternary_calculate import TernaryCalculate as tc
@@ -16,6 +15,38 @@ class AntisolventCalculate:
         Moreover, validates the antisolvent's effectivenss by calculating the solubility difference.
         """
 
-    def create_system(self) -> Type(tc):
+    def create_system(self) -> tc:
+        """
+        Creates a system based on the class TernaryCalculate.
+        Check the TernaryCalculate script for more info.
+        """
         system = tc()
         return system
+
+    def calc_ternary_data(self, system: tc, **kwarg) -> pandas.DataFrame:
+        """
+        Calculates the ternary data for the given system.
+
+        Paramters
+        =========
+        system: TernaryCalculate
+            The system with solute, solvent, antisolvent.
+
+        Returns
+        =======
+        DataFrame
+            DataFrame with solute, solvent, antisolvent mole fraction.
+
+        Note
+        ====
+        See TernaryCalculate.calculate for kwarg info.
+        """
+        ternary_data = system.calculate(
+            ngrid=kwarg.get("ngrid", 21), trace=kwarg.get("trace", True)
+        )
+        ternary_data = pandas.DataFrame(
+            ternary_data,
+            index=["solute mole frac", "solvent mole frac", "antisolvent mole frac"],
+        )
+
+        return ternary_data
